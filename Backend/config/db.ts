56bc91fba55f4f79c4dbcbf5aa46a2c716135155
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-07-30 02:37:47 +0530

    Color palette

--- Backend/config/db.ts
+++ Backend/config/db.ts
@@ -1,14 +1,17 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = () => {
-    mongoose.connect(process.env.DB_URL||"")
+  mongoose
+    .connect(
+      process.env.DB_URL ||
+        "mongodb+srv://sandeepyadav004343:tunnoh-8tatsa-zuwKev@cluster.spnaci6.mongodb.net/"
+    )
     .then(() => {
-        console.log("MongoDB is Connected...");
+      console.log("MongoDB is Connected...");
     })
-    .catch(err => {
-        console.error('DB connection failed:', err);
+    .catch(() => {
+      console.error("DB connection failed:");
+    });
+};
 
-    })
-}
-
-export default connectDB
+export default connectDB;
